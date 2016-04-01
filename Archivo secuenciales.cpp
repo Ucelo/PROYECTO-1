141b45e86375f6eaa6861ade311234919b8bfd9b
Ucelo <tony.ucelo0607@gmail.com>
2016-04-01 12:46:49 -0600

    Added files via upload

---
+++ Archivo secuenciales.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Base, Altura;
+    float Area;
+
+    cout << "Ingrese Base: ";
+    cin >> Base;
+    cout << "Ingrese Altura: ";
+    cin >> Altura;
+
+    Area=Base*Altura/2;
+
+    cout << "EL AREA ES; " << endl << Area;
+
+    	return 0;
+}
